1f413797e9d87a67c8a55298f74b4c2dbc1fb40f
Junior Mabunda <mjmabunda0@gmail.com>
2024-07-13 08:51:53 +0200

    Finalize desktop design initial version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Handle the mobile menu toggle functionality.
 let toggle = document.getElementById("menu-toggle");
 toggle.addEventListener("click", function() {
     document.getElementById("nav-links").classList.toggle("closed");
